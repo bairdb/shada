687c348927f264726238cd0bcac1421aa2424039
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2011-11-02 04:28:08 -0500

    update

--- lib/shada/shada_cli.rb
+++ lib/shada/shada_cli.rb
@@ -1,16 +1,19 @@
-@tasks = {}
-
 module Shada
-  module CLI
-    def self.included(base)
-      self.instance_eval(base)
-      @task = ARGV[0]
+  class CLI
+    
+    def self.cli &block
+      task = ARGV[0]
+      @@tasks = {}
+      klass = self.new
+      if block_given?
+        klass.instance_eval &block
+      end
+      puts @@tasks
     end
     
     def task arg, &block
-      @tasks[arg] = block
+      @@tasks[arg] = block
     end
   end
 end
 
-include Shada::CLI
