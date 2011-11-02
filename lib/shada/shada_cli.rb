e33b99c86bea573e3a8726d1bfe8783d9ba03627
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2011-11-02 14:01:34 -0500

    update

--- lib/shada/shada_cli.rb
+++ lib/shada/shada_cli.rb
@@ -8,7 +8,31 @@ module Shada
       if block_given?
         klass.instance_eval &block
       end
-      @@tasks[task.to_sym].call ARGV
+      @@tasks[task.to_sym].call parse
+    end
+    
+    def parse
+      args = {}
+      args_arr = []
+      args_dict = {}
+      
+      i=0
+      ARGV.each do |arg|
+        if i > 0
+          arg_p = arg.split(':')
+          if arg_p.count > 1
+            args_dict[arg_p[0]] = arg_p[1]
+          else
+            args_arr.push(arg)
+          end
+        end
+        i = i+1
+      end
+      
+      args[:dict] = args_dict
+      args[:arr] = args_arr
+      
+      return args
     end
     
     def task arg, &block
