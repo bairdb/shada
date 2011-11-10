000237e091f1db68d7ef8f63316fec077d303ff1
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2011-11-10 01:33:42 -0600

    update

--- lib/shada/shada_image.rb
+++ lib/shada/shada_image.rb
@@ -11,10 +11,35 @@ module Shada
       @img_ext = image[1]
     end
     
-    def thumbnail width, height
-      puts @img
+    def thumbnail width, height, path=""
       tmp_img = @img.resize_to_fill width, height
       tmp_img.write "#{Shada::Config['ThumbPath']}#{@img_name}_thumb.#{@img_ext}"
+    end
+    
+    def resize width, height, path=""
+      tmp_img = @img.resize_to_fit width, height
+      tmp_img.write "#{Shada::Config['ImagePath']}#{@img_name}.#{@img_ext}"
+    end
+    
+    def scale percent, path=""
+      tmp_img = @img.scale percent
+      tmp_img.write "#{Shada::Config['ImagePath']}#{@img_name}_scale_#{percent}.#{@img_ext}"
+    end
+    
+    def format
+      @img.format
+    end
+    
+    def filesize
+      @img.filesize
+    end
+    
+    def width
+      @img.columns
+    end
+    
+    def height
+      @img.rows
     end
   end
 end
