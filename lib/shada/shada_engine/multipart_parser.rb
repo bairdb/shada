3792a80bd2ab61854446b2a42029aee0adb3bdee
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2011-12-07 01:01:50 -0600

    update

--- lib/shada/shada_engine/multipart_parser.rb
+++ lib/shada/shada_engine/multipart_parser.rb
@@ -33,7 +33,7 @@ module Shada
       @isDisp = false
       @isType = false
       
-      test = IO.readlines(file)
+      test = IO.read(file)
       #puts "IO: #{test}"
       
       #File.open(file, 'rb')
@@ -97,15 +97,15 @@ module Shada
             next
           end
           
-          #unless @isDisp
+          unless @isDisp
             if @filename
               @tmp << line
             else
               @tmp << line.chomp
             end
-          #else
-          #  @isDisp = false
-          #end
+          else
+            @isDisp = false
+          end
           
         rescue => e
           next
