9af2a8fa8be9a436a809af0f6a3c7f88c47795ce
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2011-12-08 01:43:31 -0600

    update

--- lib/shada/shada_upload.rb
+++ lib/shada/shada_upload.rb
@@ -26,14 +26,14 @@ module Shada
           parser = Shada::Multipart_Parser.new.parse tmpf
           
           parser.form_fields.each do |k,v|
-            puts "#{k} - #{v}"
             @form.set_header k, v, 'post'
           end
           
           parser.files.each do |k, v|
-            puts "#{k} - #{v}"
             @form.set_header k, v, 'file'
           end
+          
+          puts @form[:file]
         else
           save_file upload, @headers['PATH']
         end
@@ -51,12 +51,10 @@ module Shada
           parser = Shada::Multipart_Parser.new.parse tmpf
           
           parser.form_fields.each do |k,v|
-            puts "#{k} - #{v}"
             @form.set_header k, v, 'post'
           end
           
           parser.files.each do |k, v|
-            puts "#{k} - #{v}"
             @form.set_header k, v, 'file'
           end
         else
