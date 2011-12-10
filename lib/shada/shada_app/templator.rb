8aae86d3fba6a92052239b957e76d5af26b96f81
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2011-12-10 01:28:40 -0600

    update

--- lib/shada/shada_app/templator.rb
+++ lib/shada/shada_app/templator.rb
@@ -164,7 +164,7 @@ module Shada
      @html.scan(@result_pattern).inject(1) do |i, result|
        @rep_pattern = @content_arr[i - 1].to_s.strip
        @tmp = ""
-       @html = @html.gsub "%%replacement_#{i}%%", @content_arr[i - 1]
+       @html = @html.gsub "%%replacement_#{i}%%", @content_arr[i - 1] unless @html.nil?
        @content_arr.delete_at(i - 1)
        
        #preprocess_results
