2ee5a0a47bec47b846144bf86682a314a29c7d4f
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2011-12-19 19:33:43 -0600

    udpate

--- lib/shada/shada_engine/headers.rb
+++ lib/shada/shada_engine/headers.rb
@@ -65,6 +65,7 @@ module Shada
     
     def set_header key, val, type='response'
       key = key.to_s.gsub(/\"/, '').to_sym
+      puts key
       case type
       when 'get'
         @get[key] = val
