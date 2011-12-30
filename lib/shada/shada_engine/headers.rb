5c6025b5c8a6372c2dad966b1db353dfe297db78
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2011-12-29 23:39:52 -0600

    update

--- lib/shada/shada_engine/headers.rb
+++ lib/shada/shada_engine/headers.rb
@@ -87,11 +87,11 @@ module Shada
       @cookies[key]
     end
     
-    def set_cookie key, val, expires=nil, path=nil, domain=nil, secure=nil
+    def set_cookie key, val, expires=nil, path='/', domain=nil, secure=nil
       cookie = "#{key}=#{val}"
       cookie = "#{cookie}; domain=#{Shada::Config['Host']}"
       cookie = "#{cookie}; expires=#{rfc2822(expires.clone.gmtime)}" unless expires.nil?
-      cookie = "#{cookie}; path=#{path}" unless path.nil?
+      cookie = "#{cookie}; path=#{path}"
       cookie = "#{cookie}; #{secure}" unless secure.nil?
       @outgoing_cookies.push cookie
       @response_headers['Set-Cookie'] = @outgoing_cookies.join(', ')
