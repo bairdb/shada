077231e923a65cbba03509771e16919e9a7759de
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2012-01-10 19:33:53 -0600

    update

--- lib/shada/shada_image.rb
+++ lib/shada/shada_image.rb
@@ -33,23 +33,23 @@ module Shada
     end
     
     def resize2 width, height, save=true, path=""
-      img_size = {:main =>{:cols => width,:rows => height}}
+      path = path.nil? ? Shada::Config['ImagePath'] : path
       
-      main_image = @img.change_geometry!("#{img_size[:main][:cols]}x#{img_size[:main][:rows]}") do |cols, rows, img|
-       if cols < img_size[:main][:cols] || rows < img_size[:main][:rows]
+      main_image = @img.change_geometry("#{width}x#{height}") do |cols, rows, img|
+       if cols < width || rows < height
         img.resize!(cols, rows)
-        bg = Magick::Image.new(img_size[:main][:cols],img_size[:main][:rows]){self.background_color = "white"}
+        bg = Magick::Image.new(width,height){self.background_color = "white"}
         bg.composite(img, Magick::CenterGravity, Magick::OverCompositeOp)
        else
           img.resize!(cols, rows)
        end
       end
-      
-      if save
-        main_image.write "#{path}#{@img_name}.#{@img_ext}" unless File.exists? "#{Shada::Config['ImagePath']}#{@img_name}.#{@img_ext}"
-      else
-        main_image.to_blob
-      end
+      puts main_image
+      #if save
+      #  main_image.write "#{path}#{@img_name}.#{@img_ext}" unless File.exists? "#{Shada::Config['ImagePath']}#{@img_name}.#{@img_ext}"
+      #else
+      #  main_image.to_blob
+      #end
     end
     
     def scale percent, save=true, path=""
