32c2cfbcda9fb0bdeb1057e1f933bafbf5e98415
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2012-01-13 23:01:37 -0600

    update

--- pkg/shada-0.0.1/lib/shada/shada_data/mysql2.rb
+++ pkg/shada-0.0.1/lib/shada/shada_data/mysql2.rb
@@ -69,7 +69,7 @@ module Shada
         else
 
           result.each do |r|
-            obj = self.class.dup
+            obj = self.dup
             r.each do |field, val|
               obj.instance_variable_set("@#{field}", val)
             end
