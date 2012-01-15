466a62e4d2e96eb0940440cbc353b43c975eb8df
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2012-01-15 13:40:16 -0600

    update

--- lib/shada/shada_engine/multipart_parser.rb
+++ lib/shada/shada_engine/multipart_parser.rb
@@ -69,7 +69,7 @@ module Shada
                   f.close
                 end
                 
-                @files[@name] = {:filename => @filename, :type => FILE_TYPES[@type], :path => @p}
+                @files[@name] = {:filename => @filename, :type => FILE_TYPES[@type], :path => @p, :file_type => @type}
                 @filename =  nil
                 @body = []
               end
@@ -93,7 +93,7 @@ module Shada
                   f.close
                 end
                 
-                @files[@name] = {:filename => @filename, :type => FILE_TYPES[@type], :path => @p}
+                @files[@name] = {:filename => @filename, :type => FILE_TYPES[@type], :path => @p, :file_type => @type}
                 @filename =  nil
                 @body = []
               end
