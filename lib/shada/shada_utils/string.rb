c24c9a3c68ef900f802f9f6ded0094d41d57be2a
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2012-02-07 15:55:08 -0600

    update

--- lib/shada/shada_utils/string.rb
+++ lib/shada/shada_utils/string.rb
@@ -2,4 +2,8 @@ class String
   def propercase
     self.split(/\s+/).each{|word|word.capitalize!}.join(' ')
   end
+  
+  def big_chomp
+    self.strip.chomp.gsub(/\n+/m," ").gsub(/\t+/m, " ").gsub(/\s+/m, ' ').gsub(/\r+/m, ' ')
+  end
 end
