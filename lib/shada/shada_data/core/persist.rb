b3fc15091e42b0e4c1e03bfc4895bb1e39f981ee
Baird Lackner-Buckingham <baird@lackner-buckingham.com>
2012-05-22 00:41:04 -0500

    update

--- lib/shada/shada_data/core/persist.rb
+++ lib/shada/shada_data/core/persist.rb
@@ -4,15 +4,21 @@ module Shada
   module Data
     module Persist
       def persist file_name, obj, dir=""
+        if File.exists?()
+          FileUtils.chmod 0777, "#{@dir}#{file_name}"
+        end
+        
         @dir = dir
         File.open("#{@dir}#{file_name}","wb") do |file|
            Marshal::dump(obj,file)
         end
+        
         FileUtils.chmod 0777, "#{@dir}#{file_name}"
       end
       
       def flush file_name, dir=""
         @dir = dir
+        FileUtils.chmod 0777, "#{@dir}#{file_name}"
         File.open("#{@dir}#{file_name}","wb")
       end
 
